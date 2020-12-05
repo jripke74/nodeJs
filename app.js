04c2b13939dabc288828fd9bb58d725a5ff4a5e1
jripke74 <jripke74@gmail.com>
2020-12-05 08:03:29 -0800

    13 adding a 404 error page

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(adminRoutes);
 app.use(shopRoutes);
 
-app.use('/', (req, res, next) => {
-  res.send('<h1>Hello from Express!');
-});
+app.use((req, res, next) => {
+  res.status(404).send('<h1>Page not found</h1>');
+})
 
 app.listen(3000);
