71fb83de0ae66e724cf29b0a4769015f01960b80
jripke74 <jripke74@gmail.com>
2020-12-05 07:47:40 -0800

    12 splitting our routing code up

--- app.js
+++ app.js
@@ -3,18 +3,13 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/add-product', (req, res, next) => {
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="titel"><button type="submit">Add Product</button></form>'
-  );
-});
-
-app.post('/product', (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/');
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.use('/', (req, res, next) => {
   res.send('<h1>Hello from Express!');
