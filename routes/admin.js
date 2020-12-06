b3d38c0d120a6dea33ecc31959dfb1c71edf1314
jripke74 <jripke74@gmail.com>
2020-12-06 08:20:02 -0800

    13 working on the layout with partials

--- routes/admin.js
+++ routes/admin.js
@@ -10,7 +10,10 @@ const products = [];
 
 // /admin/add-product => GEt
 router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+  });
 });
 
 // /admin/add-product => POST
