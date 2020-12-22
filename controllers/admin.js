9ab29a22bb677f67925b809438f5daa5a0bd9c0a
jripke74 <jripke74@gmail.com>
2020-12-22 07:32:48 -0800

    13 adding the product delete functionality

--- controllers/admin.js
+++ controllers/admin.js
@@ -4,7 +4,7 @@ exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
-    editing: false
+    editing: false,
   });
 };
 
@@ -12,8 +12,8 @@ exports.postAddProduct = (req, res, next) => {
   const title = req.body.title;
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
-  const description = req.body.description
-  const product = new Product(title, imageUrl, price, description);
+  const description = req.body.description;
+  const product = new Product(null, title, imageUrl, description, price);
   product.save();
   res.redirect('/');
 };
@@ -24,7 +24,7 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findById(prodId, product => {
+  Product.findById(prodId, (product) => {
     if (!product) {
       return res.redirect('/');
     }
@@ -32,13 +32,26 @@ exports.getEditProduct = (req, res, next) => {
       pageTitle: 'Add Product',
       path: '/admin/edit-product',
       editing: editMode,
-      product: product
+      product: product,
     });
   });
 };
 
 exports.postEditProduct = (req, res, next) => {
-
+  const prodId = req.body.productId;
+  const updatedTitle = req.body.title;
+  const updatedPrice = req.body.price;
+  const updatedImageUrl = req.body.imageUrl;
+  const updatedDesc = req.body.description;
+  const updatedProduct = new Product(
+    prodId,
+    updatedTitle,
+    updatedImageUrl,
+    updatedDesc,
+    updatedPrice
+  );
+  updatedProduct.save();
+  res.redirect('/admin/products');
 };
 
 exports.getProducts = (req, res, next) => {
@@ -49,4 +62,8 @@ exports.getProducts = (req, res, next) => {
       path: '/admin/products',
     });
   });
+};
+
+exports.postDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
 };
