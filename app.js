36c8c7fed37027954fac75ffbd67f8e702c66ba5
jripke74 <jripke74@gmail.com>
2020-12-27 12:13:28 -0800

    09 fetching products

--- app.js
+++ app.js
@@ -14,23 +14,6 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-db.execute('SELECT * FROM products')
-  .then((result) => {
-    console.log(result[0], result[1]);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-
-// CREATE TABLE `node-complete`.`products` (
-//   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-//   `title` VARCHAR(255) NOT NULL,
-//   `price` DOUBLE NOT NULL,
-//   `description` TEXT NOT NULL,
-//   `imageUrl` VARCHAR(255) NOT NULL,
-//   PRIMARY KEY (`id`),
-//   UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE);
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
