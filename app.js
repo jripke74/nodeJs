fd78ad70c4ad80d096b19958bc0d9fe86cad8f22
jripke74 <jripke74@gmail.com>
2020-12-27 11:53:05 -0800

    08 retrieving data

--- app.js
+++ app.js
@@ -14,7 +14,13 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-db.execute('SELECT * FROM products').then().catch();
+db.execute('SELECT * FROM products')
+  .then((result) => {
+    console.log(result[0], result[1]);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 // CREATE TABLE `node-complete`.`products` (
 //   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
