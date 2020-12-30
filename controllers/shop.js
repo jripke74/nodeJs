fa24f16783bda703d5bb360592338cce37c294a4
jripke74 <jripke74@gmail.com>
2020-12-29 16:38:40 -0800

    07 retrieving data finding products

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,12 +2,12 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
-    .then(([rows, fieldData]) => {
+  Product.findAll()
+    .then((products) => {
       res.render('shop/product-list', {
-        prods: rows,
+        prods: products,
         pageTitle: 'All Products',
-        path: '/products',
+        path: '/products'
       });
     })
     .catch((err) => {
@@ -22,7 +22,7 @@ exports.getProduct = (req, res, next) => {
       res.render('shop/product-detail', {
         product: product[0],
         pageTitle: product.title,
-        path: '/products'
+        path: '/products',
       });
     })
     .catch((err) => {
@@ -31,12 +31,12 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll()
-    .then(([rows, fieldData]) => {
+  Product.findAll()
+    .then((products) => {
       res.render('shop/index', {
-        prods: rows,
+        prods: products,
         pageTitle: 'Shop',
-        path: '/',
+        path: '/'
       });
     })
     .catch((err) => {
