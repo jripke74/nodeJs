305582e2cd0d6c1a3b6cff62e7f8adf4bedc3acb
jripke74 <jripke74@gmail.com>
2021-01-07 17:05:45 -0800

    21 working on cart items and orders

--- models/user.js
+++ models/user.js
@@ -4,14 +4,30 @@ const getDb = require("../utilities/database").getDb;
 const ObjectId = mongodb.ObjectID;
 
 class User {
-  constructor(username, email) {
+  constructor(username, email, cart, id) {
     this.name = username;
     this.email = email;
+    this.catch = cart;
+    this._id = id;
   }
 
   save() {
     const db = getDb();
     return db.collection("users").insertOne(this);
+  }
+
+  addToCart(product) {
+    // const cartProduct = this.cart.items.findIndex(cp => {
+    //   return cp._id === product._id;
+    // });
+    const updatedCart = { items: [{ ...product, quantity: 1 }] };
+    const db = getDb();
+    return db
+      .collection("users")
+      .updateOne(
+        { _id: new ObjectId(this._id) },
+        { $set: { cart: updatedCart } }
+      );
   }
 
   static findById(userId) {
