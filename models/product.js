ec32f69ca8c175f0d531dd701de762c6ce4989d8
jripke74 <jripke74@gmail.com>
2021-01-13 17:07:08 -0800

    05 saving data through mongoose

--- models/product.js
+++ models/product.js
@@ -11,7 +11,7 @@ const productSchema = new Schema({
     type: Number,
     required: true
   },
-  descrition: {
+  description: {
     type: String,
     required: true
   },
@@ -20,6 +20,8 @@ const productSchema = new Schema({
     required: true
   }
 });
+
+module.exports = mongoose.model('Product', productSchema);
 
 // const mongodb = require("mongodb");
 // const getDb = require("../utilities/database").getDb;
