16310e1cc80d072b348758633ace3d225b497c64
Jeff Ripke <jeff@Jeffs-MacBook-Pro.local>
2021-01-28 10:22:32 -0800

    add options to mongoose.connect in app.js

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
-const path = require("path");
+const path = require('path');
 
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
-const errorController = require("./controllers/error");
-const User = require("./models/user");
+const errorController = require('./controllers/error');
+const User = require('./models/user');
 
 const app = express();
 
-app.set("view engine", "ejs");
-app.set("views", "views");
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  User.findById("6005a7b6935e3dc39912217d")
+  User.findById('6005a7b6935e3dc39912217d')
     .then((user) => {
       req.user = user;
       next();
@@ -28,25 +28,26 @@ app.use((req, res, next) => {
     .catch((err) => console.log(err));
 });
 
-app.use("/admin", adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
 
 app.use(errorController.get404);
 
 mongoose
-  .connect(
-    "mongodb+srv://jeff:uftyMz9t5jXpts4r@nodejstrn.yhsnj.mongodb.net/shop?retryWrites=true&w=majority"
-  )
+  .connect('mongodb://localhost:27017/shop', {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+  })
   .then((result) => {
     User.findOne().then((user) => {
       if (!user) {
         const user = new User({
-          name: "Jeff",
-          email: "jeff@jeff.com",
+          name: 'Jeff',
+          email: 'jeff@jeff.com',
           cart: {
-            items: [],
-          },
+            items: []
+          }
         });
         user.save();
       }
