3bab43aa1a6aa9819b2bcd09240a7e2106fcb00f
Jeff Ripke <jripke74@gmail.com>
2021-02-03 15:34:09 -0800

    18 making add to cart work again

--- controllers/shop.js
+++ controllers/shop.js
@@ -65,7 +65,7 @@ exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
   Product.findById(prodId)
     .then((product) => {
-      return req.session.user.addToCart(product);
+      return req.user.addToCart(product);
     })
     .then((result) => {
       console.log(result);
@@ -84,7 +84,7 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
-  req.session.user
+  req.user
     .populate('cart.items.productId')
     .execPopulate()
     .then((user) => {
