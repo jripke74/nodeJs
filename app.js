39ec8ade923e613bc1a616a6e08e26239d597618
Jeff Ripke <jripke74@gmail.com>
2021-02-03 18:17:40 -0800

    05 implementing an authentication flow

--- app.js
+++ app.js
@@ -59,18 +59,6 @@ mongoose
     useNewUrlParser: true
   })
   .then((result) => {
-    User.findOne().then((user) => {
-      if (!user) {
-        const user = new User({
-          name: 'Jeff',
-          email: 'jeff@jeff.com',
-          cart: {
-            items: []
-          }
-        });
-        user.save();
-      }
-    });
     app.listen(3000);
   })
   .catch((err) => {
--- controllers/auth.js
+++ controllers/auth.js
@@ -9,12 +9,12 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.getSignup = (req, res, next) => {
-  res.render('auth/login', {
-    path: '/login',
-    pageTitle: 'Login',
+  res.render('auth/signup', {
+    path: '/signup',
+    pageTitle: 'Signup',
     isAuthenticated: false
-  })
-}
+  });
+};
 
 exports.postLogin = (req, res, next) => {
   User.findById('601190f15929c1251f680e63')
@@ -30,7 +30,27 @@ exports.postLogin = (req, res, next) => {
 };
 
 exports.postSignup = (req, res, next) => {
-
+  const email = req.body.email;
+  const password = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
+  User.findOne({ email: email })
+    .then((userDoc) => {
+      if (userDoc) {
+        return res.redirect('/signup');
+      }
+      const user = new User({
+        email: email,
+        password: password,
+        cart: { items: [] }
+      });
+      return user.save();
+    })
+    .then(result => {
+      res.redirect('/login');
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 exports.postLogout = (req, res, next) => {
