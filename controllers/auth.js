a6b9b14cc46195fbc929b45e453cca38699a1ec2
Jeff Ripke <jripke74@gmail.com>
2021-02-18 16:24:10 -0800

    05 creating the reset password form

--- controllers/auth.js
+++ controllers/auth.js
@@ -152,7 +152,7 @@ exports.postReset = (req, res, next) => {
         user.resetTokenExpiration = Date.now() + 3600000;
         return user.save();
       })
-      .then(result => {
+      .then((result) => {
         res.redirect('/');
         transporter.sendMail({
           to: req.body.email,
@@ -168,4 +168,26 @@ exports.postReset = (req, res, next) => {
         console.log(err);
       });
   });
+};
+
+exports.getNewPassword = (req, res, next) => {
+  const token = req.params.token;
+  User.findOne({ resetToken: token, resetTokenExpiration: { $gt: Date.now() } })
+    .then((user) => {
+      let message = req.flash('error');
+      if (message.length > 0) {
+        message = message[0];
+      } else {
+        message = null;
+      }
+      res.render('auth/new-password', {
+        path: '/new-password',
+        pageTitle: 'New Password',
+        errorMessage: message,
+        userId: user._id.toString()
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
